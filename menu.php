f89700066045d431ea85182cd94bcdd58640b910
afdolsing <singgih.student@gmail.com>
2020-07-18 06:37:51 +0700

    update

--- menu.php
+++ menu.php
@@ -5,8 +5,7 @@
 <ul class="nav">
     <?php
         $menu = mysqli_query($conn, "SELECT * FROM blog_menu ORDER BY thread");
-        while ($data = mysqli_fetch_array($menu)) :
-    ?>
+        while ($data = mysqli_fetch_array($menu)) : ?>
         <li>
             <a href="<?= $data['link'] ?>"><?= $data['title'] ?></a>
             <ul>
